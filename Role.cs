5fb011de450d543624663fb6c790e8f821cad7b4
Tran Thanh Xuan <ttxuan0595@gmail.com>
2023-12-17 10:40:29 +0700

    first commit

---
+++ Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class ROLE
+    {
+        public static string STAFF = "STAFF";
+        public static string ADMIN = "ADMIN";
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Staff:Person
+    {
+        public Staff(String id, string fullname, string account, string password): base(id, fullname, account, password) {
+            this.Role = ROLE.STAFF; 
+        }
+    
+
+    }
+}
